7bdf9fcd5b41366c3cc29a9900a31fb890c1fd6c
George <Jc82200172@gmail.com>
2019-07-17 15:45:50 -0700

    2nd commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var inquirer = require("inquirer");
+
+var Word = require("./Word.js");
+var figlet = require('figlet');
+var chalk = require('chalk');
+
+
--- words.js
+++ words.js
@@ -1,2 +1,28 @@
 var Latter = require("./letter.js")
 
+var Word = function (word) {
+    this.buildWorkd = function (word){
+        for (var i = 0; i < word.lenth; i++){
+            var currentLetter = new Letter(would[i]);
+            lettersStore.push(currentLetter)
+        }
+        return lettersStore;
+    }
+    this.letters = this.builWord(word);
+    this.choseWord = word;
+
+    this.checkGuess = function (guess){
+        
+        for (var i = 0; i < this.letters.lenth; i++){
+            this.letters [i].letterGuess(guesse)
+        }
+    }
+    this.display = function (){
+        var lettersStore = '';
+        for (var i = 0; i <this.letters.length; i++){
+            lettersStore += this.letters[i].display();
+        }
+        return lettersStore;
+    }
+module.exports = Word;
+}
